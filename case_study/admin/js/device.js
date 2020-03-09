d194d43c7bf3fc676a9e310003eacf65b49cca71
Trung Nguyen <xuantrung48@gmail.com>
2020-03-09 13:44:28 +0700

    update

--- case_study/admin/js/device.js
+++ case_study/admin/js/device.js
@@ -31,21 +31,28 @@ device.initBrand = function() {
 device.save = function() {
 	if($("#formAddEditDevice").valid()) {
 		if($("#id").val() == 0) {
-			var deviceObj		= {};
-			deviceObj.Name		= $("#Name").val();
-			deviceObj.Images	= [];
+			var deviceObj			= {};
+			deviceObj.Name			= $("#Name").val();
+			deviceObj.Images		= [];
 			if ($("#Images0").val() != "")
 				deviceObj.Images.push($("#Images0").val());
 			if ($("#Images1").val() != "")
 				deviceObj.Images.push($("#Images1").val());
 			if ($("#Images2").val() != "")
 				deviceObj.Images.push($("#Images2").val());
-			var brandObj		= {};
-			brandObj.id			= $("#Brand").val();
-			brandObj.Name		= $("#Brand option:selected").html();
-			deviceObj.Brand		= brandObj;
-			deviceObj.Status	= $("#Status").val();
-			deviceObj.Price		= $("#Price").val();
+			var brandObj			= {};
+			brandObj.id				= $("#Brand").val();
+			brandObj.Name			= $("#Brand option:selected").html();
+			deviceObj.Brand			= brandObj;
+			deviceObj.Status		= $("#Status").val();
+			deviceObj.Price			= $("#Price").val();
+			deviceObj.CPU			= $("#CPU").val();
+			deviceObj.Screen		= $("#Screen").val();
+			deviceObj.OS			= $("#OS").val();
+			deviceObj.RearCamera	= $("#RearCamera").val();
+			deviceObj.FrontCamera	= $("#FrontCamera").val();
+			deviceObj.Ram			= $("#Ram").val();
+			deviceObj.Rom			= $("#Rom").val();
 			$.ajax({
 				url: "http://localhost:3000/Devices",
 				method: "POST",
@@ -57,23 +64,31 @@ device.save = function() {
 					device.drawTable();
 				}
 			});
+			bootbox.alert("Device added!");
 		} else {
-			var deviceObj		= {};
-			deviceObj.id		= $("#id").val();
-			deviceObj.Name		= $("#Name").val();
-			deviceObj.Images	= [];
+			var deviceObj			= {};
+			deviceObj.id			= $("#id").val();
+			deviceObj.Name			= $("#Name").val();
+			deviceObj.Images		= [];
 			if ($("#Images0").val() != "")
 				deviceObj.Images.push($("#Images0").val());
 			if ($("#Images1").val() != "")
 				deviceObj.Images.push($("#Images1").val());
 			if ($("#Images2").val() != "")
 				deviceObj.Images.push($("#Images2").val());
-			var brandObj		= {};
-			brandObj.id			= $("#Brand").val();
-			brandObj.Name		= $("#Brand option:selected").html();
-			deviceObj.Brand		= brandObj;
-			deviceObj.Status	= $("#Status").val();
-			deviceObj.Price		= $("#Price").val();
+			var brandObj			= {};
+			brandObj.id				= $("#Brand").val();
+			brandObj.Name			= $("#Brand option:selected").html();
+			deviceObj.Brand			= brandObj;
+			deviceObj.Status		= $("#Status").val();
+			deviceObj.Price			= $("#Price").val();
+			deviceObj.CPU			= $("#CPU").val();
+			deviceObj.Screen		= $("#Screen").val();
+			deviceObj.OS			= $("#OS").val();
+			deviceObj.RearCamera	= $("#RearCamera").val();
+			deviceObj.FrontCamera	= $("#FrontCamera").val();
+			deviceObj.Ram			= $("#Ram").val();
+			deviceObj.Rom			= $("#Rom").val();
 			$.ajax({
 				url: "http://localhost:3000/Devices/" + deviceObj.id,
 				method: "PUT",
@@ -85,6 +100,7 @@ device.save = function() {
 					device.drawTable();
 				}
 			});
+			bootbox.alert("Device updated!");
 		}
 	}
 }
@@ -110,6 +126,13 @@ device.reset = function() {
 	$("#Brand").val('');
 	$("#Status").val('');
 	$("#Price").val('');
+	$("#CPU").val('');
+	$("#Screen").val('');
+	$("#OS").val('');
+	$("#RearCamera").val('');
+	$("#FrontCamera").val('');
+	$("#Ram").val('');
+	$("#Rom").val('');
 	var validator = $("#formAddEditDevice").validate();
 	validator.resetForm();
 }
@@ -127,7 +150,7 @@ device.drawTable = function() {
 					imagesDevice += "<a href='" + v.Images[i] + "'data-toggle='lightbox' data-gallery='gallery" + v.id + "'><img src='" + v.Images[i] + "' height='50'></a> ";
 				}
 				$("#tbDevices").append(
-					"<tr><td>" + v.id + "</td><td>" + v.Name + "</td><td>" + imagesDevice + "</td><td>" + v.Brand.Name + "</td><td>" + v.Status + " %</td><td>" + device.displayPriceWithDot(v.Price) + " ₫</td><td><a href='javascript:void(0);' onclick='device.get(" + v.id + ")'><i class='fa fa-edit' title='Edit this device'></i></a> <a href='javascript:void(0);' onclick='device.remove(" + v.id + ")'><i class='fa fa-trash' title='Remove this device'></i></a></td></tr>"
+					"<tr><td>" + v.id + "</td><td>" + v.Name + "</td><td>" + imagesDevice + "</td><td>" + v.Brand.Name + "</td><td>" + v.CPU + "</td><td>" + v.Screen + "</td><td>" + v.OS + "</td><td>" + v.RearCamera + "</td><td>" + v.FrontCamera + "</td><td>" + v.Ram + " GB</td><td>" + v.Rom + " GB</td><td>" + v.Status + " %</td><td>" + device.displayPriceWithDot(v.Price) + " ₫</td><td><a href='javascript:void(0);' onclick='device.get(" + v.id + ")'><i class='fa fa-edit' title='Edit this device'></i></a> <a href='javascript:void(0);' onclick='device.remove(" + v.id + ")'><i class='fa fa-trash' title='Remove this device'></i></a></td></tr>"
 				);
 			})
 		}
@@ -150,6 +173,13 @@ device.get = function(id) {
 			$("#Brand").val(id);
 			$("#Status").val(data.Status);
 			$("#Price").val(data.Price);
+			$("#CPU").val(data.CPU);
+			$("#Screen").val(data.Screen);
+			$("#OS").val(data.OS);
+			$("#RearCamera").val(data.RearCamera);
+			$("#FrontCamera").val(data.FrontCamera);
+			$("#Ram").val(data.Ram);
+			$("#Rom").val(data.Rom);
 			var validator = $("#formAddEditDevice").validate();
 			validator.resetForm();
 			$('#myModal').modal('show');
@@ -179,6 +209,7 @@ device.remove = function(id) {
 						device.drawTable();
 					}
 				});
+				bootbox.alert("Device removed!");
 			}
 		}
 	});
