5662dd6f28aeee746eaec44cfcbe8710a22d700a
Trung Nguyen <xuantrung48@gmail.com>
2020-03-31 11:32:35 +0700

    first upload

---
+++ CSharp/Netcore_CSharp/TinhDienTichHinhChuNhat/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TinhDienTichHinhChuNhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter rectangle's width: ");
+            float width = float.Parse(Console.ReadLine());
+            Console.Write("Enter rectangle's height: ");
+            float height = float.Parse(Console.ReadLine());
+            Console.WriteLine("The area of the rectangle is " + areaRectangle(width, height));
+            Console.Read();
+        }
+        static double areaRectangle(float w, float h)
+        {
+            return w * h;
+        }
+    }
+}
