050338f8a87c0adb679fa21a705427902005eb53
Trung Nguyen <xuantrung48@gmail.com>
2020-03-31 12:06:18 +0700

    first upload

---
+++ CSharp/Netcore_CSharp/KiemTraNamNhuan/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KiemTraNamNhuan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(year + " is " + (isLeapYear(year) ? "a leap year." : "not a leap year"));
+        }
+        static bool isLeapYear(int y)
+        {
+            if ((y % 4 == 0 && y % 100 != 0) || (y % 100 == 0 && y % 400 == 0))
+                return true;
+            return false;
+        }
+    }
+}
