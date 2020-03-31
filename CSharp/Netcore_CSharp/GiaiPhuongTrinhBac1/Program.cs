36c6ddcd96641ff8783fa5298520e482d13b3e82
Trung Nguyen <xuantrung48@gmail.com>
2020-03-31 17:29:28 +0700

    first upload

---
+++ CSharp/Netcore_CSharp/GiaiPhuongTrinhBac1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GiaiPhuongTrinhBac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Giai phuong trinh bac 1 ax + b = 0");
+            Console.Write("Nhap vao a: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("Nhap vao b: ");
+            float b = float.Parse(Console.ReadLine());
+            if (a != 0)
+                Console.WriteLine("Phuong trinh co 1 nghiem x = " + (-b / a) + ".");
+            else
+            {
+                if (b == 0)
+                    Console.WriteLine("Phuong trinh co vo so nghiem.");
+                else
+                    Console.WriteLine("Phuong trinh vo nghiem.");
+            }
+        }
+    }
+}
