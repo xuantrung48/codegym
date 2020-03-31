58d8cdd8fa2337ac7f6d5f131121948f0c4778c4
Trung Nguyen <xuantrung48@gmail.com>
2020-03-31 11:09:44 +0700

    update

---
+++ CSharp/Netcore_CSharp/CheckPrimeNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CheckPrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(isPrime(n) ? n + " is a prime number." : n + " is not a prime number.");
+            Console.Read();
+        }
+        static bool isPrime(int number)
+        {
+            if (number < 2)
+                return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
