1ee6e3d49ead9a5aed691d7f5fbc9f2796e10567
Trung Nguyen <xuantrung48@gmail.com>
2020-03-31 17:33:54 +0700

    first upload

---
+++ CSharp/Netcore_CSharp/KhaiBaoVaSuDungBien/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace KhaiBaoVaSuDungBien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            float f = 20.5f;
+
+            double d = 20.5;
+
+            Boolean b = true;
+
+            char c = 'a';
+
+            String s = "CodeGym";
+
+            Console.WriteLine("Gia tri cua bien i la " + i);
+
+            Console.WriteLine("Gia tri cua bien f la " + f);
+
+            Console.WriteLine("Gia tri cua bien d la " + d);
+
+            Console.WriteLine("Gia tri cua bien b la " + b);
+
+            Console.WriteLine("Gia tri cua bien c la " + c);
+
+            Console.WriteLine("Gia tri cua bien s la " + s);
+        }
+    }
+}
