1bae99254450da0b89dea910db6d6d90da621793
Trung Nguyen <xuantrung48@gmail.com>
2020-03-31 14:53:45 +0700

    first commit

---
+++ CSharp/Netcore_CSharp/TimUocSoChungLonNhat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TimUocSoChungLonNhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao so dau tien:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap vao so thu 2: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a == 0 || b == 0)
+                Console.WriteLine("Khong co uoc so chung lon nhat.");
+            else
+            {
+                while (a != b)
+                {
+                    if (a > b)
+                        a -= b;
+                    else
+                        b -= a;
+                }
+                Console.WriteLine("Uoc so chung lon nhat la: " + a);
+            }
+        }
+    }
+}
