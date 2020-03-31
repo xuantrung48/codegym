314da0b529cd9512c04e250d23fec13cb11cdc01
Trung Nguyen <xuantrung48@gmail.com>
2020-03-31 14:39:56 +0700

    first commit

---
+++ CSharp/Netcore_CSharp/TinhTienLai/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TinhTienLai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao so von ban dau: ");
+            float i = int.Parse(Console.ReadLine());
+            Console.Write("Nhap vao lai suat: ");
+            float r = float.Parse(Console.ReadLine());
+            Console.Write("Nhap vao so thang cho vay: ");
+            float t = int.Parse(Console.ReadLine());
+            Console.WriteLine("So tien lai la: " + tinhTienLai(i, r, t));
+        }
+        static float tinhTienLai(float i, float r, float t)
+        {
+            return i * r / 100 / 12 * 3;
+        }
+    }
+}
