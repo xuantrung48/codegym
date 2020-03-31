f766babc7d8478051529a01116b26b5b74951dc9
Trung Nguyen <xuantrung48@gmail.com>
2020-03-31 18:10:07 +0700

    first commit

---
+++ CSharp/Netcore_CSharp/HienThiLoiChao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HienThiLoiChao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.Read();
+        }
+    }
+}
