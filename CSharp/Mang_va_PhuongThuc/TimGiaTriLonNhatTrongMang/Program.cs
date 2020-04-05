b6c29eb5e0ac43c7c402a273b4e7d119ddf8a664
Trung Nguyen <xuantrung48@gmail.com>
2020-04-05 20:11:54 +0700

    first commit

---
+++ CSharp/Mang_va_PhuongThuc/TimGiaTriLonNhatTrongMang/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TimGiaTriLonNhatTrongMang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            int[] array;
+            do
+            {
+                Console.Write("Enter a size: ");
+                size = Int32.Parse(Console.ReadLine());
+                if (size > 20)
+                    Console.WriteLine("Size should not exceed 20");
+            } while (size > 20);
+            array = new int[size];
+            int i = 0;
+            while (i < array.Length)
+            {
+                Console.Write("Enter element " + (i + 1) + ": ");
+                array[i] = Int32.Parse(Console.ReadLine());
+                i++;
+            }
+            Console.Write("Property list: ");
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.Write(array[j] + " ");
+            }
+            Console.WriteLine();
+            int max = array[0];
+            int index = 1;
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (array[j] > max)
+                {
+                    max = array[j];
+                    index = j + 1;
+                }
+            }
+            Console.WriteLine("The largest property value in the list is " + max + ", at position " + index);
+        }
+    }
+}
