14958f7dd4693b0e3488bcfdbedc03ed2dc7da74
Trung Nguyen <xuantrung48@gmail.com>
2020-04-07 12:45:55 +0700

    first commit

---
+++ CSharp/LopVaDoiTuong/LopHinhChuNhat/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LopHinhChuNhat
+{
+    class Rectangle
+    {
+        private double width;
+        private double height;
+
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(double w, double h)
+        {
+            this.width = w;
+            this.height = h;
+        }
+
+        public double GetArea()
+        {
+            return this.width * this.height;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * (this.width + this.height);
+        }
+        public void Display()
+        {
+            Console.WriteLine("Rectangle {" + "width = " + this.width + ", height = " + this.height + "}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the width: ");
+            Double.TryParse(Console.ReadLine(), out double width);
+            Console.Write("Enter the height: ");
+            Double.TryParse(Console.ReadLine(), out double height);
+            Rectangle rectangle = new Rectangle(width, height);
+            Console.WriteLine("Area: " + rectangle.GetArea());
+            Console.WriteLine("Perimeter: " + rectangle.GetPerimeter());
+            rectangle.Display();
+        }
+    }
+}
---
+++ CSharp/Mang_va_PhuongThuc/TimPhanTuLonNhatTrongMang2Chieu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimPhanTuLonNhatTrongMang2Chieu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
