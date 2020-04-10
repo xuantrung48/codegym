ace2e49ebf9ab7b7abe28867617995f5a3c9a8c8
Trung Nguyen <xuantrung48@gmail.com>
2020-04-10 16:34:48 +0700

    first commit

---
+++ CSharp/BaiTapOOP/BaiTapOOP/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaiTapOOP
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DoB { get; set; }
+        public string Address { get; set; }
+
+        public Employee(string name, string email, DateTime dob, string address)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.DoB = dob;
+            this.Address = address;
+            Console.WriteLine("_______________________________");
+        }
+        public int GetAge()
+        {
+            int currentYear = DateTime.Now.Year;
+            return DateTime.Now.Year - this.DoB.Year;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: " + this.Name);
+            Console.WriteLine("Email: " + this.Email);
+            Console.WriteLine("Date of Birth: " + this.DoB);
+            Console.WriteLine("Age: " + this.GetAge());
+            Console.WriteLine("Address: " + this.Address);
+        }
+    }
+}
---
+++ CSharp/BaiTapOOP/BaiTapOOP/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace BaiTapOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int option = 0;
+
+            Employee[] employees = new Employee[0];
+            do
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Create employees array");
+                Console.WriteLine("2. Show employees");
+                Console.WriteLine("3. Search employee");
+                Console.WriteLine("4. Exit");
+                if (Int32.TryParse(Console.ReadLine(), out int num))
+                {
+                    option = num;
+                }
+                Console.Clear();
+                Process(option, ref employees);
+            } while (option != 4);
+        }
+        static void Process(int option, ref Employee[] employees)
+        {
+            switch (option)
+            {
+                case 1:
+                    CreateEmployeeArray(ref employees);
+                    break;
+                case 2:
+                    ShowEmployees(employees);
+                    break;
+                case 3:
+                    SearchEmployees(employees);
+                    break;
+            }
+        }
+        static void SearchEmployees(Employee[] employees)
+        {
+            Console.Write("Enter the name to search: ");
+            string nameToSearch = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("No\tName\tEmail\tDoB\tAddress");
+            int counter = 0;
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i].Name.ToLower().Contains(nameToSearch.ToLower()))
+                {
+                    counter++;
+                    Console.WriteLine(counter + ".\t" + employees[i].Name + "\t" + employees[i].Email + "\t" + employees[i].DoB.ToString("dd/MM/yyyy") + "\t" + employees[i].Address);
+                }
+            }
+            if (counter == 0)
+                Console.WriteLine("Not found.");
+        }
+        static void ShowEmployees(Employee[] employees)
+        {
+            Console.WriteLine("Employees list: ");
+            Console.WriteLine("No\tName\tEmail\tDoB\tAddress");
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine(i + 1 + ".\t" + employees[i].Name + "\t" + employees[i].Email + "\t" + employees[i].DoB.ToString("dd/MM/yyyy") + "\t" + employees[i].Address);
+            }
+        }
+        static void CreateEmployeeArray(ref Employee[] employees)
+        {
+            int n = 1;
+            do
+            {
+                Console.WriteLine("Create an employee:");
+                Array.Resize(ref employees, employees.Length + 1);
+                employees[employees.Length - 1] = CreateEmployee();
+                Console.Write("Enter 0 to exit! or any to create another employee: ");
+                if (Int32.TryParse(Console.ReadLine(), out int num))
+                {
+                    n = num;
+                }
+                Console.Clear();
+            } while (n != 0);
+        }
+        static Employee CreateEmployee()
+        {
+            Console.Write("Employee's name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Date of Birth: ");
+            DateTime.TryParse(Console.ReadLine(), out DateTime dob);
+            Console.Write("Address: ");
+            string address = Console.ReadLine();
+            string nameToLower = name.ToLower();
+            Employee newEmployee = new Employee(name, email, dob, address);
+            return newEmployee;
+        }
+    }
+}
