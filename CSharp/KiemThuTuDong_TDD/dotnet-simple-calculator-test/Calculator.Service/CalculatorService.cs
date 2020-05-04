0fb244f125372e7286f1cbe95ccf428b266e12bb
Trung Nguyen <xuantrung48@gmail.com>
2020-05-04 10:39:41 +0700

    first commit

---
+++ CSharp/KiemThuTuDong_TDD/dotnet-simple-calculator-test/Calculator.Service/CalculatorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator.Service
+{
+    public class CalculatorService
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Sub(int first, int second)
+        {
+            return first - second;
+        }
+    }
+}
---
+++ CSharp/KiemThuTuDong_TDD/dotnet-simple-calculator-test/Calculator.Test/CalculatorTest.cs
@@ -0,0 +1,108 @@
+using NUnit.Framework;
+using Calculator.Service;
+
+namespace Calculator.Test
+{
+    public class CalculatorTest
+    {
+        [TestFixture]
+        public class PrimeService_IsPrimeShould
+        {
+            private CalculatorService _calculatorService;
+
+            [SetUp]
+            public void SetUp()
+            {
+                _calculatorService = new CalculatorService();
+            }
+
+            [Test]
+            public void TestAdd0And0()
+            {
+                int first = 0;
+                int second = 0;
+                int expected = 0;
+
+                int result = _calculatorService.Add(first, second);
+                Assert.AreEqual(expected, result);
+            }
+
+            [Test]
+            public void TestAdd1And0()
+            {
+                int first = 1;
+                int second = 0;
+                int expected = 1;
+
+                int result = _calculatorService.Add(first, second);
+                Assert.AreEqual(expected, result);
+            }
+
+            [Test]
+            public void TestAdd0And1()
+            {
+                int first = 0;
+                int second = 1;
+                int expected = 1;
+
+                int result = _calculatorService.Add(first, second);
+                Assert.AreEqual(expected, result);
+            }
+
+            [Test]
+            public void TestAdd5And3()
+            {
+                int first = 5;
+                int second = 3;
+                int expected = 8;
+
+                int result = _calculatorService.Add(first, second);
+                Assert.AreEqual(expected, result);
+            }
+
+            [Test]
+            public void TestSub0and0()
+            {
+                int first = 0;
+                int second = 0;
+                int expected = 0;
+
+                int result = _calculatorService.Sub(first, second);
+                Assert.AreEqual(expected, result);
+            }
+
+            [Test]
+            public void TestSub1and0()
+            {
+                int first = 1;
+                int second = 0;
+                int expected = 1;
+
+                int result = _calculatorService.Sub(first, second);
+                Assert.AreEqual(expected, result);
+            }
+
+            [Test]
+            public void TestSub0and1()
+            {
+                int first = 0;
+                int second = 1;
+                int expected = -1;
+
+                int result = _calculatorService.Sub(first, second);
+                Assert.AreEqual(expected, result);
+            }
+
+            [Test]
+            public void TestSub5and3()
+            {
+                int first = 5;
+                int second = 3;
+                int expected = 2;
+
+                int result = _calculatorService.Sub(first, second);
+                Assert.AreEqual(expected, result);
+            }
+        }
+    }
+}
