a8023c87fca504887bc6fe3c15e373a50dfc134d
Trung Nguyen <xuantrung48@gmail.com>
2020-05-12 21:04:58 +0700

    first commit

---
+++ CSharp/ThuatToanSapXep/ASP_insertion_sort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ASP_insertion_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 11, 13, 5, 6 };
+            InsertionSort(arr);
+            PrintArray(arr);
+        }
+        static void InsertionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                // Move elements of arr[0..i-1], 
+                // that are greater than key, 
+                // to one position ahead of 
+                // their current position 
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+        static void PrintArray(int[] arr)
+        {
+            int i;
+            for (i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
