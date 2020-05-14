6eb3cbf600327ffd69b646dac80a66af8835f08b
Trung Nguyen <xuantrung48@gmail.com>
2020-05-14 16:54:48 +0700

    update

---
+++ CSharp/FileIO/BaiTap/FileIO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace FileIO
+{
+    class Matrix
+    {
+        public int[,] Numbers;
+        public string path;
+        public string inputFileName;
+        public string outputFileName;
+        public Matrix(string path, string inputFileName, string outputFileName)
+        {
+            this.path = path;
+            this.inputFileName = inputFileName;
+            this.outputFileName = outputFileName;
+            ReadInputFile();
+        }
+        public void ReadInputFile()
+        {
+            using StreamReader sr = (File.OpenText(path + inputFileName));
+            string rowColLine = sr.ReadLine();
+            string[] rowCol = rowColLine.Split(" ");
+            Numbers = new int[int.Parse(rowCol[0]), int.Parse(rowCol[1])];
+
+            for (int i = 0; i < Numbers.GetLength(0); i++)
+            {
+                string matrixLine = sr.ReadLine();
+                string[] matrixRowValues = matrixLine.Split(" ");
+                for (int j = 0; j < Numbers.GetLength(1); j++)
+                {
+                    Numbers[i, j] = int.Parse(matrixRowValues[j]);
+                }
+            }
+        }
+        public void WriteOutputFile()
+        {
+            using StreamWriter sr = (File.CreateText(path + outputFileName));
+            sr.WriteLine($"Tổng giá trị trong ma trận: {Helper.SumOfMatrix(Numbers)}");
+            sr.WriteLine($"Số lượng số nguyên tố trong ma trận: {Helper.NumberOfPrimeNumbers(Numbers)}");
+            sr.WriteLine($"Số lượng số lẻ trong ma trận: {Helper.NumberOfOddNumbers(Numbers)}");
+            sr.WriteLine($"Tổng giá trị các đường biên: {Helper.SumOfBorders(Numbers)}");
+            sr.WriteLine($"Ma trận với các giá trị * 3: ");
+            int[,] newMatrix = Helper.MultiWith(Numbers, 3);
+            for (int i = 0; i < newMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < newMatrix.GetLength(1); j++)
+                    sr.Write(newMatrix[i, j] + "\t");
+                sr.WriteLine();
+            }
+        }
+    }
+}
---
+++ CSharp/FileIO/BaiTap/Helper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace FileIO
+{
+    class Helper
+    {
+        public static bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+            for (int i = 2; i < Math.Sqrt(number); i++)
+                if (number % i == 0)
+                    return false;
+            return true;
+        }
+        public static int NumberOfPrimeNumbers(int[,] matrix)
+        {
+            int result = 0;
+            foreach (var number in matrix)
+                if (IsPrime(number))
+                    result++;
+            return result;
+        }
+        public static int SumOfMatrix(int[,] matrix)
+        {
+            int sum = 0;
+            foreach (var number in matrix)
+                sum += number;
+            return sum;
+        }
+        public static int NumberOfOddNumbers(int[,] matrix)
+        {
+            int result = 0;
+            foreach (var number in matrix)
+                if (number % 2 != 0)
+                    result++;
+            return result;
+        }
+        public static int SumOfBorders(int[,] matrix)
+        {
+            int result = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (i == 0 || i == matrix.GetLength(0) - 1)
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                        result += matrix[i, j];
+                else
+                    result += matrix[i, 0] + matrix[i, matrix.GetLength(1) - 1];
+            }
+            return result;
+        }
+        public static int[,] MultiWith(int[,] matrix, int number)
+        {
+            int[,] newMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+            for (int i = 0; i < newMatrix.GetLength(0); i++)
+                for (int j = 0; j < newMatrix.GetLength(1); j++)
+                    newMatrix[i, j] = matrix[i, j] * number;
+            return newMatrix;
+        }
+    }
+}
